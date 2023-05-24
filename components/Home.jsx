b332dd506ed2e6652321fe8d1c067f9ef652ba92
Prince <pribenis@gmail.com>
2023-05-24 09:07:16 +0200

    v1

---
+++ components/Home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from '../styles/Home.module.css';
+import NewArticle from './NewArticles';
+import Article from './Article';
+// import Header from './Header';
+// import Footer from './Footer';
+
+export default function Home() {
+
+  const articlesData = [
+    {
+      nameArticle: 'PHANTOM I White',
+      descArticle: 'A class of its own.',
+      url: './images/phantom1.png'
+    },
+    {
+      nameArticle: 'PHANTOM II Gold',
+      descArticle: 'Pure sound.',
+      url: './images/phantom2.png'
+    },
+    {
+      nameArticle: 'PHANTOM II Black',
+      descArticle: 'Deeply nomadic.',
+      url: './images/phantom3.png'
+    },
+    {
+      nameArticle: 'PHANTOM II White',
+      descArticle: 'Intensively immersive.',
+      url: './images/phantom4.png'
+    },
+  ];
+
+  const Articles = articlesData.map((data, i)=>{
+    return <Article
+    key = {i}{...data}
+    />
+  })
+
+  return <>
+    <NewArticle title = 'DIONE SOUNDBAR' />
+    <div className={styles.articlesContainer}>{Articles}</div>
+
+  </>;
+
+}
---
+++ components/NewArticles.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from '../styles/NewArticle.module.css';
+
+export default function NewArticle(props) {
+    return (
+        <div className={styles.newArticleContainer}>
+            <div className={styles.titleContainer}>
+                <p className={styles.newArticlesLauch}>LATEST LAUNCH</p>
+                <p className={styles.newArticlesTitle}>{props.title}</p>
+            </div>
+            <img className={styles.soundbar} src="/images/soundbar.png" alt="DIONE SOUNDBAR" />
+            <button className='discover'>DISCOVER</button>
+        </div>
+    )
+}
